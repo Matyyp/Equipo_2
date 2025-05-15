4ae10e57d9fce4514b3d625acb68b282ad7db8b1
FrvnM <97007356+FrvnM@users.noreply.github.com>
2025-05-15 16:18:07 -0400

    actualizacion ruta landing
    moví la landing a la carpeta tenant

--- routes/tenant.php
+++ routes/tenant.php
@@ -46,7 +46,7 @@ Route::middleware([
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
     Route::get('/', function () {
-        return view('welcome');
+        return view('tenant.landings.welcome');
     });
     //aca agregar todos los roles que necesiten entran al panel de admin
     Route::middleware(['auth', 'permission:admin.panel.access'])->group(function () {
